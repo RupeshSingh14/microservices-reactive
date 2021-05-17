6d121f385608a7798d25f60b1a96c65fb940b8a3
Rupesh Singh <rupeshkumarsingh.14@gmail.com>
2021-05-17 22:00:33 +0530

    added Eureka service discovery

---
+++ service-discovery-server/src/test/java/com/singh/rupesh/ServiceDiscovery/ServiceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singh.rupesh.ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
